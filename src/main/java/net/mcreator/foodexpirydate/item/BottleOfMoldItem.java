066bd91c24c69696df96e1500c1b6e2c5450876e
Sekaus <christian.foged.ludvigsen@hotmail.com>
2026-01-31 10:41:48 +0100

    Create BottleOfMoldItem.java

---
+++ src/main/java/net/mcreator/foodexpirydate/item/BottleOfMoldItem.java
@@ -0,0 +1,53 @@
+package net.mcreator.foodexpirydate.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+
+import net.mcreator.foodexpirydate.procedures.MoldyMilkRightclickedProcedure;
+import net.mcreator.foodexpirydate.procedures.FoodPoisoningProcedureProcedure;
+
+public class BottleOfMoldItem extends Item {
+	public BottleOfMoldItem() {
+		super(new Item.Properties().stacksTo(1).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(2).saturationMod(0.5f).alwaysEat().build()));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.DRINK;
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		MoldyMilkRightclickedProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ());
+		return ar;
+	}
+
+	@Override
+	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+		ItemStack retval = new ItemStack(Items.GLASS_BOTTLE);
+		super.finishUsingItem(itemstack, world, entity);
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		FoodPoisoningProcedureProcedure.execute(world, x, y, z, entity);
+		if (itemstack.isEmpty()) {
+			return retval;
+		} else {
+			if (entity instanceof Player player && !player.getAbilities().instabuild) {
+				if (!player.getInventory().add(retval))
+					player.drop(retval, false);
+			}
+			return itemstack;
+		}
+	}
+}
